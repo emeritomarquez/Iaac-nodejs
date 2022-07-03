a5874fee6d4217b494e17c5c6d10db3480fb7a96
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-07-03 10:32:06 +0000

    Add dst-ip.py to git tracking

--- dst-ip.py
+++ dst-ip.py
@@ -6,11 +6,13 @@ f = open(sys.argv[1], 'r')
 lines = f.readlines()
 f.close()
 rows = []
+
 for line in lines:
     if line =! "[hosts]\n":
         rows.append(line)
 print(rows)
 g = open(sys.argv[2], 'w+')
+
 for element in rows:
     g.write(element)
 g.close()
